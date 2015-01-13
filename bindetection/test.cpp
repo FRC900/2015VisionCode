bf4fdf8dbafeb955acdcee02d3f5b1f243be96cd
Kevin Jaget <kjaget@nc.rr.com>
2015-01-13 11:10:36 -0500

    Added classifer_bin_3 results Updated gitignore to exclude backups & binaries Modified test.cpp  - Ability to save detected images - useful for grabbing false positives to add to next pass of training  - Quick and dirty attempt to ID bins using histograms.  Not great but does help a bit  - Fixed code to use full detected image rather than bottom half  - Various cleanup in detection parameters - should find bins more consistently with these

--- bindetection/test.cpp
+++ bindetection/test.cpp
@@ -9,73 +9,27 @@
  using namespace cv;
 
  /** Function Headers */
- void detectAndDisplay( Mat frame );
+ void detectAndDisplay( Mat frame, vector<Mat> &images );
 
  /** Global variables */
- String face_cascade_name = "cascade.xml";
+ String face_cascade_name = "classifier_bin_3/cascade.xml";
  CascadeClassifier face_cascade;
  string window_name = "Capture - Face detection";
 
- int scale     = 20;
- int neighbors = 30;
- int min_row   = 40;
- int min_col   = 50;
- int max_row   = 400 * 4;
- int max_col   = 500 * 4;
- int r_min     = 65;
- int r_max     = 90;
- int b_min     = 100;
- int b_max     = 170;
+ int scale     = 3;
+ int neighbors = 2;
+ int min_row   = 20;
+ int min_col   = 20;
+ int max_row   = 200 * 4;
+ int max_col   = 200 * 4;
+ //int r_min     = 65;
+ //int r_max     = 90;
+ //int b_min     = 100;
+ //int b_max     = 170;
  int hist_divider = 1;
- /** @function main */
-int main( int argc, const char** argv )
-{
-   VideoCapture cap(0);
-   Mat frame;
-   Mat frame_copy;
 
-   bool pause = false;
 
-   namedWindow("Parameters", cv::WINDOW_AUTOSIZE);
-   createTrackbar ("Scale", "Parameters", &scale, 200, NULL);
-   createTrackbar ("Neighbors", "Parameters", &neighbors, 500, NULL);
-  // createTrackbar ("Min Row", "Parameters", &min_row, 1000, NULL);
-  // createTrackbar ("Min Col", "Parameters", &min_col, 1000, NULL);
-  // createTrackbar ("Max Row", "Parameters", &max_row, 1000, NULL);
-  // createTrackbar ("Max Col", "Parameters", &max_col, 1000, NULL);
-   createTrackbar ("R Min", "Parameters", &r_min, 256, NULL);
-   createTrackbar ("R Max", "Parameters", &r_max, 256, NULL);
-   createTrackbar ("B Min", "Parameters", &b_min, 256, NULL);
-   createTrackbar ("B Max", "Parameters", &b_max, 256, NULL);
-   createTrackbar ("Hist Divider", "Parameters", &hist_divider, 16, NULL);
-
-   //-- 1. Load the cascades
-   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
-
-   //-- 2. Read the video stream
-   while( true )
-   {
-      if (!pause)
-      {
-	 cap >> frame;
-	 frame_copy = frame.clone();
-	
-      }
-      else
-	 frame = frame_copy.clone();
-         pyrDown(frame, frame);
-
-      //-- 3. Apply the classifier to the frame
-      if( !frame.empty() )
-      { detectAndDisplay( frame ); }
-      else
-      { printf(" --(!) No captured frame -- Break!"); break; }
-      int c = waitKey(1);
-      if( (char)c == 'c' ) { break; }
-      if( c == ' ') pause = !pause;
-   }
-      return 0;
- }
+// Convert type value to a human readable string. Useful for debug
 string type2str(int type) {
   string r;
 
@@ -99,143 +53,247 @@ string type2str(int type) {
   return r;
 }
 
+void writeImage(const vector<Mat> &images, size_t index, const char *path, int frameCount)
+{
+   if (index < images.size())
+   {
+      // Create filename, save image
+      stringstream fn;
+      fn << path;
+      fn << "_";
+      fn << frameCount;
+      fn << "_";
+      fn << index;
+      fn << ".png";
+      imwrite(fn.str().substr(fn.str().rfind('\\')+1), images[index]);
+      // Save 20x20 version of the same image
+      fn.clear(); 
+      fn << path;
+      fn << "_";
+      fn << frameCount;
+      fn << "_";
+      fn << index;
+      fn << "_s.png";
+      Mat smallImg;
+      resize(images[index], smallImg, Size(20,20));
+      imwrite(fn.str().substr(fn.str().rfind('\\')+1), smallImg);
+   }
+}
+
+int main( int argc, const char** argv )
+{
+   VideoCapture cap(argv[1]);
+   Mat frame;
+   Mat frame_copy;
+   vector <Mat> images;
+   int frameCount = 0;
+
+   bool pause = false;
+
+   namedWindow("Parameters", WINDOW_AUTOSIZE);
+   createTrackbar ("Scale", "Parameters", &scale, 200, NULL);
+   createTrackbar ("Neighbors", "Parameters", &neighbors, 500, NULL);
+   createTrackbar ("Min Row", "Parameters", &min_row, 1000, NULL);
+   createTrackbar ("Min Col", "Parameters", &min_col, 1000, NULL);
+   createTrackbar ("Max Row", "Parameters", &max_row, 1000, NULL);
+   createTrackbar ("Max Col", "Parameters", &max_col, 1000, NULL);
+   //createTrackbar ("R Min", "Parameters", &r_min, 256, NULL);
+   //createTrackbar ("R Max", "Parameters", &r_max, 256, NULL);
+   //createTrackbar ("B Min", "Parameters", &b_min, 256, NULL);
+   //createTrackbar ("B Max", "Parameters", &b_max, 256, NULL);
+   createTrackbar ("Hist Divider", "Parameters", &hist_divider, 16, NULL);
+
+   //-- 1. Load the cascades
+   if( !face_cascade.load( face_cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
+
+   //-- 2. Read the video stream
+   while( true )
+   {
+      if (!pause)
+      {
+	 cap >> frame;
+	 frame_copy = frame.clone();
+	 frameCount += 1;
+      }
+      else
+	 frame = frame_copy.clone();
+
+      //-- 3. Apply the classifier to the frame
+      if( !frame.empty() )
+      { detectAndDisplay( frame, images ); }
+      else
+      { printf(" --(!) No captured frame -- Break!"); break; }
+      int c = waitKey(5);
+      if( c == 'c' ) { break; } // exit
+      else if( c == ' ') { pause = !pause; }
+      else if( c == 'f')  // advance to next frame
+      {
+	 cap >> frame;
+	 frame_copy = frame.clone();
+	 frameCount += 1;
+      }
+      else if (c == 'a') // save all detected images
+      {
+	 for (size_t index = 0; index < images.size(); index++)
+	    writeImage(images, index, argv[1], frameCount);
+      }
+      else if (isdigit(c)) // save a single detected image
+      {
+	 writeImage(images, c - '0',  argv[1], frameCount);
+      }
+   }
+      return 0;
+}
+
  float range[] = { 0, 256 } ;
   const float* histRange = { range };
   bool uniform = true; bool accumulate = false;
 /** @function detectAndDisplay */
-void detectAndDisplay( Mat frame )
+void detectAndDisplay( Mat frame, vector<Mat> &images )
 {
   std::vector<Rect> faces;
   Mat frame_gray;
-int histSize = 256 / (hist_divider ? hist_divider : 1);
+  Mat frame_copy;
+  frame_copy = frame.clone();
+  images.clear();
+  int histSize = 256 / (hist_divider ? hist_divider : 1);
 
   cvtColor( frame, frame_gray, CV_BGR2GRAY );
   equalizeHist( frame_gray, frame_gray );
 
   //-- Detect faces
-  face_cascade.detectMultiScale( frame_gray, faces, std::max(1.05,(double)scale/20.), neighbors, 0|CV_HAAR_SCALE_IMAGE, Size(min_row, min_col), Size(max_row, max_col) );
+  face_cascade.detectMultiScale( frame_gray, faces, 1.05 + scale/100., neighbors, 0|CV_HAAR_SCALE_IMAGE, Size(min_row, min_col), Size(max_row, max_col) );
 
-  for( size_t i = 0; i < faces.size(); i++ )
+  // Mark and save up to 10 detected images
+  for( size_t i = 0; i < min(faces.size(), (size_t)10); i++ )
   {
-    vector<Mat> bgr_planes;
-    Mat smallImg;
-    stringstream winName;
-   winName << "Face "; 
-    winName << i;
-    string winNameStr = winName.str();
-    frame(Rect(faces[i].x, faces[i].y + faces[i].height*1/2, faces[i].width, faces[i].height/2)).copyTo(smallImg);
-    split (smallImg, bgr_planes);
-    bool uniform = true; bool accumulate = false;
+     // Copy detected image into images[i]
+     // Copy from frame_copy so that ID rectangles written onto frame are excluded
+     images.push_back(Mat());
+     frame_copy(Rect(faces[i].x, faces[i].y, faces[i].width, faces[i].height)).copyTo(images[i]);
 
-    //cvtColor(smallImg, smallImg, COLOR_BGR2HSV);
-    Mat hist[3];
-    double min_val[3];
-    double max_val[3];
-    int min_idx[3];
-    int max_idx[3];
+     // Split into individual B,G,R channels so we can run a histogram on each
+     vector<Mat> bgr_planes;
+     split (images[i], bgr_planes);
+     bool uniform = true; bool accumulate = false;
 
-    for (size_t j = 0; j < 3; j++)
-    {
-       /// Compute the histograms:
-       calcHist( &bgr_planes[j], 1, 0, Mat(), hist[j], 1, &histSize, &histRange, uniform, accumulate );
+     //cvtColor(images[i], images[i], COLOR_BGR2HSV);
+     Mat hist[3];
+     double min_val[3];
+     double max_val[3];
+     //int min_idx[3];
+     int max_idx[3];
 
-       hist[j].at<float>(255) = 0.;
-       hist[j].at<float>(0) = 0.;
-       Point min, max;
-       minMaxLoc(hist[j], min_val, max_val + j, &min, &max);
-       min_idx[j] = min.y;
-       max_idx[j] = max.y;
-       //cout << i << " " << type2str(hist[j].type()) << " " << j << " " << max_idx[j] << endl;
-	
-    }
-/*
-double ratioBG, ratioBR, ratioGR;
-ratioBG = (double)max_idx[0] / max_idx[1];
-ratioBR = (double)max_idx[0] / max_idx[2];
-ratioGR = (double)max_idx[1] / max_idx[2];
-cout << "BG Ratio: " << ratioBG;
-cout << "   BR Ratio: " << ratioBR;
-cout << "   GR Ratio: " << ratioGR << endl;
-if (max_idx[0] > max_idx[2] && max_idx[1] > max_idx[2])
-rectangle( frame, faces[i], Scalar( 0, 0, 255 ),3);
+     for (size_t j = 0; j < 3; j++)
+     {
+	/// Compute the histograms:
+	calcHist( &bgr_planes[j], 1, 0, Mat(), hist[j], 1, &histSize, &histRange, uniform, accumulate );
 
-*/
-    if ((max_idx[0] + max_idx[1]) < max_idx[2])
-    {
-       cout << "R hit " ;
-       for (size_t j = 0; j < 3; j++)
-	  cout << " " << max_idx[j];
-       cout << endl;
-       rectangle( frame, faces[i], Scalar( 0, 0, 255 ),3);
-    }
-    else if ((max_idx[1] + max_idx[2]) < max_idx[0])
-    {
-       cout << "B hit " ;
-       for (size_t j = 0; j < 3; j++)
-	  cout << " " << max_idx[j];
-       cout << endl;
-       rectangle( frame, faces[i], Scalar( 255, 0, 0 ),3);
-    }
-    else if ((std::min(max_idx[0], max_idx[1]) * 1.5) < max_idx[2])    
-    {
-       cout << "R2 hit " ;
-       for (size_t j = 0; j < 3; j++)
-	  cout << " " << max_idx[j];
-       cout << endl;
-       rectangle( frame, faces[i], Scalar( 0, 128, 255 ),3);
-    }
-    else if ((std::min(max_idx[1], max_idx[2]) * 1.5) < max_idx[0])    
-    {
-       cout << "B2 hit " ;
-       for (size_t j = 0; j < 3; j++)
-	  cout << " " << max_idx[j];
-       cout << endl;
-       rectangle( frame, faces[i], Scalar( 255, 128, 0 ),3);
-    }
+	// Remove 0 & 255 intensities since these seem to confuse things later
+	hist[j].at<float>(255) = 0.;
+	hist[j].at<float>(0) = 0.;
 
-    else
-    rectangle( frame, faces[i], Scalar( 255, 0, 255 ),3);
+	// Grab the color intensity peak
+	Point min, max;
+	minMaxLoc(hist[j], min_val, max_val + j, &min, &max);
+	//min_idx[j] = min.y;
+	max_idx[j] = max.y;
+     }
 
-#if 1
-    // Draw the histograms for B, G and R
-    int hist_w = 512; int hist_h = 400;
-    int bin_w = cvRound( (double) hist_w/histSize );
-
-    Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-    /// Normalize the result to [ 0, histImage.rows ]
-    normalize(hist[0], hist[0], 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    normalize(hist[1], hist[1], 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-    normalize(hist[2], hist[2], 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-    /// Draw for each channel
-    for( int ii = 1; ii < histSize; ii++ )
-    {
-       line( histImage, Point( bin_w*(ii-1), hist_h - cvRound(hist[0].at<float>(ii-1)) ) ,
-	     Point( bin_w*(ii), hist_h - cvRound(hist[0].at<float>(ii)) ),
-	     Scalar( 255, 0, 0), 2, 8, 0  );
-       line( histImage, Point( bin_w*(ii-1), hist_h - cvRound(hist[1].at<float>(ii-1)) ) ,
-	     Point( bin_w*(ii), hist_h - cvRound(hist[1].at<float>(ii)) ),
-	     Scalar( 0, 255, 0), 2, 8, 0  );
-       line( histImage, Point( bin_w*(ii-1), hist_h - cvRound(hist[2].at<float>(ii-1)) ) ,
-	     Point( bin_w*(ii), hist_h - cvRound(hist[2].at<float>(ii)) ),
-	     Scalar( 0, 0, 255), 2, 8, 0  );
-    }
-    namedWindow(winNameStr, WINDOW_NORMAL);
-    imshow (winNameStr, smallImg);
-    winName << " Hist";
-    imshow (winName.str(), histImage);
+     cerr << i << " " << max_idx[0] << " " << max_idx[1] << " " <<max_idx[2] << " " << images[i].cols <<  " " << images[i].rows << " " << type2str(images[i].type()) << endl;
+#if 0
+     if ((max_idx[0] + max_idx[1]) < max_idx[2])
+     {
+	cout << "R hit " ;
+	for (size_t j = 0; j < 3; j++)
+	   cout << " " << max_idx[j];
+	cout << endl;
+	rectangle( frame, faces[i], Scalar( 0, 0, 255 ),3);
+     }
+     else if ((max_idx[1] + max_idx[2]) < max_idx[0])
+     {
+	cout << "B hit " ;
+	for (size_t j = 0; j < 3; j++)
+	   cout << " " << max_idx[j];
+	cout << endl;
+	rectangle( frame, faces[i], Scalar( 255, 0, 0 ),3);
+     }
+     else if ((std::min(max_idx[0], max_idx[1]) * 1.5) < max_idx[2])    
+     {
+	cout << "R2 hit " ;
+	for (size_t j = 0; j < 3; j++)
+	   cout << " " << max_idx[j];
+	cout << endl;
+	rectangle( frame, faces[i], Scalar( 0, 128, 255 ),3);
+     }
+     else if ((std::min(max_idx[1], max_idx[2]) * 1.5) < max_idx[0])    
+     {
+	cout << "B2 hit " ;
+	for (size_t j = 0; j < 3; j++)
+	   cout << " " << max_idx[j];
+	cout << endl;
+	rectangle( frame, faces[i], Scalar( 255, 128, 0 ),3);
+     }
+     else
 #endif
+	// First attempt at isolating bins : B >= R and G >= R.  
+	// This does reasonably well but there are still false positives
+	// negatives so more work is needed
+	if ((max_idx[0] >= max_idx[2]) && (max_idx[1] >= max_idx[2]))
+	{
+	   rectangle( frame, faces[i], Scalar( 0, 0, 255 ),3);
+	}
+	else
+	   rectangle( frame, faces[i], Scalar( 255, 0, 255 ),3);
 
-    if (i > 20) break;
+     // Label each outlined image with a digit.  Top-level code allows
+     // users to save these small images by hitting the key they're labeled with
+     // This should be a quick way to grab lots of falsly detected images
+     // which need to be added to the negative list for the next
+     // pass of classifier training.
+     stringstream label;
+     label << i;
+     putText( frame, label.str(), Point(faces[i].x, faces[i].y), FONT_HERSHEY_PLAIN, 1.0, Scalar(255, 255, 0));
+#if 0
+     // Draw the histograms for B, G and R
+     int hist_w = 512; int hist_h = 400;
+     int bin_w = cvRound( (double) hist_w/histSize );
+
+     Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+     /// Normalize the result to [ 0, histImage.rows ]
+     for (size_t j = 0; j < 3; j++)
+	normalize(hist[j], hist[j], 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+
+     const Scalar colors[3] = {Scalar(255,0,0), Scalar(0,255,0), Scalar(0,0,255)};
+     // For each point in the histogram
+     for( int ii = 1; ii < histSize; ii++ )
+     {
+	// Draw for each channel
+	for (size_t jj = 0; jj < 3; jj++)
+	   line( histImage, Point( bin_w*(ii-1), hist_h - cvRound(hist[jj].at<float>(ii-1)) ) ,
+		 Point( bin_w*(ii), hist_h - cvRound(hist[jj].at<float>(ii)) ),
+		 colors[jj], 2, 8, 0  );
+     }
+
+     // Display detected images and histograms of those images
+     stringstream winName;
+     winName << "Face "; 
+     winName << i;
+     namedWindow(winName.str(), CV_WINDOW_AUTOSIZE);
+     imshow (winName.str(), images[i]);
+     resizeWindow(winName.str(), images[i].cols, images[i].rows);
+     winName << " Hist";
+     imshow (winName.str(), histImage);
+#endif
   }
 #if 0
   for( size_t i = 0; i < faces.size(); i++ )
   {
-    //Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
-    rectangle( frame, faces[i], Scalar( 255, 0, 255 ),3);
+     //Point center( faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5 );
+     rectangle( frame, faces[i], Scalar( 255, 0, 255 ),3);
   }
 #endif
   //-- Show what you got
   imshow( window_name, frame );
- }
+}
 
