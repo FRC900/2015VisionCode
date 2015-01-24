5c8b7d8e793ad04b4c446817a99965dc9ee6bc0b
Kevin Jaget <kjaget@nc.rr.com>
2015-01-24 14:23:08 -0500

    Update training scripts

--- cascade_training/train.sh
+++ cascade_training/train.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-/bin/opencv_traincascade -data classifier_bin_5 -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 15 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 7000 -numNeg 5000 -mode ALL -precalcValBufSize 3000 -precalcIdxBufSize 3000
-
+/bin/opencv_traincascade -data classifier_bin_5 -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 17 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 7000 -numNeg 5000 -mode ALL -precalcValBufSize 2000 -precalcIdxBufSize 2000 -makWeakCount 500
