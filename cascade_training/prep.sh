fcd459b8a982e767b87b482503bf3596f6c41058
Kevin Jaget <kjaget@nc.rr.com>
2015-02-01 08:44:11 -0500

    Start up training with images of bin from below

--- cascade_training/prep.sh
+++ cascade_training/prep.sh
@@ -10,7 +10,7 @@ mv temp.dat negatives.dat
 /bin/find positive_images -name \*.png > positives.dat
 /bin/find positive_images -name \*.jpg >> positives.dat
 # For each positive image, create a number of randomly rotated versions of that image
-perl createtrainsamples.pl positives.dat negatives.dat . 4500 | tee foo.txt
+perl createtrainsamples.pl positives.dat negatives.dat . 7500 | tee foo.txt
 
 # Merge each set of randomized versions of the images into one big .vec file
 rm positives.vec ordered_positives.vec
@@ -20,7 +20,6 @@ mergevec/src/mergevec.exe vectors.dat ordered_positives.vec
 # Randomize the order of those images inside the .vec file
 mergevec/src/vec2img ordered_positives.vec samples%04d.png -w 20 -h 20 | shuf > info.dat
 sed 's/$/ 1 0 0 20 20/' info.dat > random_info.dat
-rm info.dat
 mergevec/src/createsamples -info random_info.dat -vec positives.vec -num `wc -l random_info.dat` -w 20 -h 20
 rm info.dat random_info.dat
 
--- cascade_training/train.sh
+++ cascade_training/train.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-/bin/opencv_traincascade -data classifier_bin_6 -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 35 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 8000 -numNeg 5000 -mode ALL -precalcValBufSize 3500 -precalcIdxBufSize 3500 -maxWeakCount 500
+/bin/opencv_traincascade -data classifier_bin_6 -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 35 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 7000 -numNeg 5000 -mode ALL -precalcValBufSize 3500 -precalcIdxBufSize 3500 -maxWeakCount 500
