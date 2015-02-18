c5d3d0328472bfd412c750eab37ddb0b6d256e49
Kevin Jaget <kjaget@nc.rr.com>
2015-02-18 09:22:14 -0500

    Start training new classifier - added positives not found from previous run

--- cascade_training/train.sh
+++ cascade_training/train.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-/bin/opencv_traincascade -data classifier_bin_6 -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 50 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 7000 -numNeg 6000 -mode ALL -precalcValBufSize 3500 -precalcIdxBufSize 3500 -maxWeakCount 500
+/bin/opencv_traincascade -data classifier_bin_7 -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 55 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 9000 -numNeg 10000 -mode ALL -precalcValBufSize 3500 -precalcIdxBufSize 3500 -maxWeakCount 500
