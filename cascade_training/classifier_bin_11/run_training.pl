38132ab4564c47f714c7786b6d195e118d62d660
Kevin Jaget <kjaget@nc.rr.com>
2015-03-07 08:03:02 -0500

    New classifier - 11 is LBP with smaller negative count to hopefully speed up initial stages making the classifier faster

---
+++ cascade_training/classifier_bin_11/run_training.pl
@@ -0,0 +1,22 @@
+while (1)
+{
+   my $pid = open(PIPE, "/bin/opencv_traincascade -data classifier_bin_11 -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 55 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 12000 -numNeg 8000 -featureType LBP -precalcValBufSize 1750 -precalcIdxBufSize 1750 -maxWeakCount 1000 |");
+   while ($line = <PIPE>)
+   {
+      print $line;
+      if ($line =~ /\|\s+(\d+)\|\s+([0-9\.]+)\|\s+([0-9\.]+\|)/)
+      {
+	 if ($1 eq "5")
+	 {
+	    `bash redo_negatives.sh`;
+	 }
+	 if ((($1 > 25) && ($2 eq "1")) || 
+	     (($1 > 75) && ($3 > 0.99)))
+	 {
+	    print "Killing\n";
+	    kill $pid;
+	    close (PIPE);
+	 }
+      }
+   }
+}
