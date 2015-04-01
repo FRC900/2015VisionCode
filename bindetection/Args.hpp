c85d768cf02e8ab0e5320d7af796384600a1ac46
Kevin Jaget <kjaget@nc.rr.com>
2015-04-01 18:35:10 -0400

    Add --calibrate to draw crosshairs on the screen. Use to align the camera on the robot

--- bindetection/Args.hpp
+++ bindetection/Args.hpp
@@ -9,10 +9,11 @@ class Args //class for processing arguments
 		bool tracking;    // display tracking info?
 		bool rects;       // display frame by frame hit info
 		bool batchMode;   // non-interactive mode - no display, run through
-		// as quickly as possible. Combine with --all
+						  // as quickly as possible. Combine with --all?
 		bool ds;          // driver-station?
 		bool writeVideo;  // write captured video to output
 		int  frameStart;  // frame number to start from
+		bool calibrate;   // crosshair to calibrate camera
 		string inputName; // input file name or camera number
 
 		Args(void);
