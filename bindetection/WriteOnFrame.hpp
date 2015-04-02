6289e00f032052870fce2b69502e37ffa98138af
Kevin Jaget <kjaget@nc.rr.com>
2015-04-02 17:25:57 -0400

    Cleanup various bits

--- bindetection/WriteOnFrame.hpp
+++ bindetection/WriteOnFrame.hpp
@@ -1,18 +1,18 @@
+#ifndef INC_WRITEONFRAME_HPP__
+#define INC_WRITEONFRAME_HPP__
+
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/objdetect/objdetect.hpp"
 #include <opencv2/opencv.hpp>
 
-using namespace std;
-using namespace cv;
-
 class WriteOnFrame {
-
 	private:
-		Mat image;
+		cv::Mat image;
 	public:
-		WriteOnFrame(const Mat &setTo);
+		WriteOnFrame(const cv::Mat &setTo);
 		void writeTime();
-		void writeMatchNumTime(string matchNum, double matchTime);
-		void write(VideoWriter &writeTo);
+		void writeMatchNumTime(std::string matchNum, double matchTime);
+		void write(cv::VideoWriter &writeTo);
 };
+#endif
