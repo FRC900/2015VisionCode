b517c687c03b9fcb23cd0cbe70f9e68aed60f385
Kevin Jaget <kjaget@nc.rr.com>
2015-05-05 22:14:06 -0400

    Clean up and documentation

--- cascade_training/make_fast_cascade.pl
+++ cascade_training/make_fast_cascade.pl
@@ -1,7 +1,23 @@
 #! /usr/bin/perl
 
+# Script to optimze the performance of early stages of a classifier.
+# It repeatedly runs the first few stages of tarining on a random
+# subset of negative input samples. For each training run, test the
+# performance by running he generated classifier on a short test
+# video. Since the classifier changes depending on the negative images
+# used, the performance of the generated classifier varies depending
+# on the quantity of filtering needed to weed out each random choice
+# of negative samples.
+#
+# The script saves the best performing of the trained classifiers.
+# Once the user is happy that they've found a quick enough preliminary
+# classifier, this saved best classifier is trained with however
+# many additional stages are needed.
+#
 my $best_fps = 0;
 
+# Read previous best FPS value. Allows restarting script
+# without losing past results.
 if (open(FPS_FILE, "classifier_bin_save/best_fps.txt"))
 {
    $line = <FPS_FILE>;
@@ -10,14 +26,21 @@ if (open(FPS_FILE, "classifier_bin_save/best_fps.txt"))
 }
 print "Previous best FPS = $best_fps\n";
 
+# This basically duplicates the work done in prep.sh
+# Generate list of positive images once since it never changes
 readpipe("/bin/find positive_images -name \*.png > positives.dat");
 while (1)
 {
+   # Grab a list of negative images and randomize their order
    print "Creating negatives\n";
    readpipe("/bin/find negative_images -name \*.png > negatives.dat");
    readpipe("shuf negatives.dat > temp.dat");
    readpipe("mv temp.dat negatives.dat");
 
+   # Each positive sample is converted into multiple images by
+   # rotating it random ways.  These are compiled into one .vec file
+   # containing all of the processed images.  Randomize the order of the
+   # positive samples inside this file.
    print "Creating sample .vecs\n";
    readpipe("rm *.vec");
    readpipe("perl createtrainsamples.pl positives.dat negatives.dat . 12000"); 
@@ -32,10 +55,12 @@ while (1)
    readpipe("mkdir -p classifier_bin_x");
    readpipe("rm -f classifier_bin_x/*");
 
+   # Train 15 stages of the classifier. This is quick enough so we don't
+   # waste too much time training but still get a reasonbly performing classifier
    print "Running classifier training\n";
    readpipe ("/bin/opencv_traincascade -data classifier_bin_x -vec positives.vec -bg negatives.dat -w 20 -h 20 -numStages 15 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos 11250 -numNeg 7000 -featureType LBP -precalcValBufSize 1750 -precalcIdxBufSize 1750 -maxWeakCount 1000");
 
-   # Run classifier on sample video
+   # Run classifier on sample video, record the speed
    print "Testing detection speed\n";
    open (FPSTEST, "../bindetection/fpstest |");
    $fps = <FPSTEST>;
@@ -44,6 +69,7 @@ while (1)
    print "$fps FPS\n";
    if ($fps > $best_fps)
    {
+      # If this is the best performing classifer, save it
       $best_fps = $fps;
       print "New best FPS\n";
       readpipe("mkdir -p classifier_bin_save");
--- cascade_training/prep.sh
+++ cascade_training/prep.sh
@@ -1,5 +1,27 @@
 #! /bin/bash
 
+# Run this once to initialize a cascade training run.  
+# Positive samples should be in the positive_images subdir.  They can be any size (the
+# script will resize the actual input to training to a uniform size) and could be color
+# or B&W.  They should all be the same aspect ratio, and that aspect ratio should
+# match the -w and -h arguments to various commands in this script.  Ideally the images
+# would show the target from a variety of angles (including elevation changes), lighting
+# conditions, against various backgrounds, etc.  The more conditions included here, the 
+# better the detection accuracy.
+# The negative_images subdir should contain images which *DO NOT* have the object
+# being detected.  These can be any size, aspect ratio, just so long as there are 
+# absolutely no examples of the object to detect are present at any size.  Be especially
+# sure this is true when grabbing "hard negatives" - false positives caputured from
+# testing intermediate stages - since sometimes the classifer will grab an image
+# which has the object included in a larger detected image.
+#
+# Note that additional negatives can be added at any time during the training process,
+# but adding positives will probably not work well.
+#
+# Things to edit :
+# - number of samples created on the "perl createtrainsamples.pl" run. More is relatively better, 
+# but more also increases trainging time.  Finding the right balance isn't an exact science.
+
 # Create list of negative images, randomize the list
 /bin/find negative_images -name \*.png > negatives.dat
 /bin/find negative_images -name \*.jpg >> negatives.dat
@@ -10,6 +32,8 @@ mv temp.dat negatives.dat
 /bin/find positive_images -name \*.png > positives.dat
 /bin/find positive_images -name \*.jpg >> positives.dat
 # For each positive image, create a number of randomly rotated versions of that image
+# This creates a .vec file for each positive input image, each containing multiple images 
+# rotated random amounts
 perl createtrainsamples.pl positives.dat negatives.dat . 12000 | tee foo.txt
 
 # Merge each set of randomized versions of the images into one big .vec file
@@ -17,7 +41,9 @@ rm positives.vec ordered_positives.vec
 /bin/find . -name \*.vec > vectors.dat
 mergevec/src/mergevec.exe vectors.dat ordered_positives.vec
 
-# Randomize the order of those images inside the .vec file
+# Randomize the order of those images inside the .vec file.
+# If you change w/h in the first line, change the numbers in the next two 
+# lines to match
 mergevec/src/vec2img ordered_positives.vec samples%04d.png -w 20 -h 20 | shuf > info.dat
 sed 's/$/ 1 0 0 20 20/' info.dat > random_info.dat
 mergevec/src/createsamples -info random_info.dat -vec positives.vec -num `wc -l random_info.dat` -w 20 -h 20
